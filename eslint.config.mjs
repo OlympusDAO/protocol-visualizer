fd3e5c407716f3d4e5380d247ceff0733180a7c1
Jem <0x0xjem@gmail.com>
2025-02-24 17:33:54 +0400

    Improved linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,10 +5,20 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   eslint.configs.recommended,
-  tseslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
   {
     rules: {
-      '@typescript-eslint/no-unused-vars': 'warn'
-    }
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/await-thenable': 'error',
+      '@typescript-eslint/no-floating-promises': 'error',
+    },
   },
 );

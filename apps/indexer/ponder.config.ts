aa18e419abc2bd474db89b950b1ffc5c924b27e2
Jem <0x0xjem@gmail.com>
2025-02-24 16:31:32 +0400

    Setup linting. Initial setup of ponder.

---
+++ apps/indexer/ponder.config.ts
@@ -0,0 +1,18 @@
+import { createConfig } from "ponder";
+import { http } from "viem";
+
+import { KernelAbi } from "./abis/Kernel";
+
+export default createConfig({
+  networks: {
+    mainnet: { chainId: 1, transport: http(process.env.PONDER_RPC_URL_1) },
+  },
+  contracts: {
+    Kernel: {
+      abi: KernelAbi,
+      address: "0x2286d7f9639e8158FaD1169e76d1FbC38247f54b",
+      network: "mainnet",
+      startBlock: 15998125,
+    },
+  },
+});

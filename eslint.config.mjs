1973b198217c872cbb7a5b8a3a6d155f9d013ce8
Jem <0x0xjem@gmail.com>
2025-02-26 12:28:20 +0400

    Fix linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,37 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
+  {
+    ignores: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/.next/**",
+      "**/build/**",
+      ".git/**",
+      "**/eslint.config.mjs",
+      "**/postcss.config.mjs",
+      "**/*.config.ts",
+      "**/next-env.d.ts",
+    ],
+  },
   eslint.configs.recommended,
   tseslint.configs.recommendedTypeChecked,
   {
     languageOptions: {
       parserOptions: {
-        projectService: true,
+        project: ["./apps/*/tsconfig.json", "./packages/*/tsconfig.json"],
         tsconfigRootDir: import.meta.dirname,
       },
     },
   },
   {
     rules: {
-      '@typescript-eslint/no-unused-vars': 'warn',
-      '@typescript-eslint/await-thenable': 'error',
-      '@typescript-eslint/no-floating-promises': 'error',
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/await-thenable": "error",
+      "@typescript-eslint/no-floating-promises": "error",
     },
-  },
+  }
 );

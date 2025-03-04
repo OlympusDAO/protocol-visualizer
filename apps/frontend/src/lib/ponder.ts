9ebe4727ecb34714d6e84d6fee48c321c733a6b8
Jem <0x0xjem@gmail.com>
2025-03-04 13:37:38 +0400

    Error if VITE_PONDER_URL is not set

--- apps/frontend/src/lib/ponder.ts
+++ apps/frontend/src/lib/ponder.ts
@@ -1,6 +1,10 @@
 import { createClient } from "@ponder/client";
 import * as schema from "../../../indexer/ponder.schema";
 
+if (!import.meta.env.VITE_PONDER_URL && import.meta.env.PROD) {
+  throw new Error("VITE_PONDER_URL is not set");
+}
+
 const client = createClient(
   `${import.meta.env.VITE_PONDER_URL || "http://localhost:42069"}/sql`,
   { schema }
